578c1ec7328e0221e787ac9268c5549c0490e13e
Lveekhout <leekhout@hotmail.com>
2015-02-07 12:27:40 +0100

    AboutDialog aangepast

--- src/com/example/dialogs/AboutDialog.java
+++ src/com/example/dialogs/AboutDialog.java
@@ -4,7 +4,6 @@ import com.example.activities.R;
 import android.os.Bundle;
 import android.app.Dialog;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.support.v4.app.DialogFragment;
 
 public class AboutDialog extends DialogFragment {
@@ -13,16 +12,12 @@ public class AboutDialog extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.DialogStr)
-               .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       System.out.println("Fire!");
-                   }
-               })
-               .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       System.out.println("Annuleren!");
-                   }
-               });
+               .setPositiveButton(R.string.gezien, null);
+//               .setPositiveButton(R.string.gezien, new DialogInterface.OnClickListener() {
+//                   public void onClick(DialogInterface dialog, int id) {
+//                       System.out.println("Fire!");
+//                   }
+//               });
         // Create the AlertDialog object and return it
         return builder.create();
     }
